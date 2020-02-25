572a2fe46569266a0e454e29f35dc02a4a221088
Alisher F <sambusa001@mail.ru>
2020-02-26 02:02:14 +0500

    feature: error system added

--- pkg/globconst/globconst.go
+++ pkg/globconst/globconst.go
@@ -2,8 +2,10 @@ package globconst
 
 const Address = "localhost:9999"
 
+const BytesInStatusCode = 3
+
 const (
-	Upload = '0'
+	Upload   = '0'
 	Download = '1'
-	List = '2'
+	List     = '2'
 )
--- pkg/status-codes/status-codes.go
+++ pkg/status-codes/status-codes.go
@@ -1,9 +1,6 @@
-package status_code
+package status_codes
 
 var (
-	SUCCESS = []byte{0, 1, 5}
+	OK              = []byte{1, 0, 0}
+	FILE_NOT_EXISTS = []byte{6, 6, 6}
 )
-
-func isStatusCodeError(code [3]byte) {
-
-}
